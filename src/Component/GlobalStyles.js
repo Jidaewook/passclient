82fca86268e2383715b6672159afea13724a3054
Jay <psatdoctor@naver.com>
2019-11-10 10:43:09 +0900

    React Router #4
    글로벌스타일스 적용, 헤더 수정

---
+++ src/Component/GlobalStyles.js
@@ -0,0 +1,26 @@
+import {createGlobalStyle} from "styled-components";
+import reset from "styled-reset";
+
+const golbalStyles = createGlobalStyle`
+    ${reset};
+    
+    ${'' /* 아래의 a는 SLink안의 링크적인 속성을 가리킨다. */}
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+
+    *{
+        box-sizing: border-box;
+    }
+
+    body{
+        font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-size: 12px;
+        background-color: rgba(20, 20, 20, 1);
+        color: white;
+        padding-top: 50px;
+    }
+`;
+
+export default globalStyles;
