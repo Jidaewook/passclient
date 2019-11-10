475abb9658ec9f8ef769fbce9143e52211594599
Jay <psatdoctor@naver.com>
2019-11-10 10:22:43 +0900

    React Router #3
    초기 스타일링. 
    헤더 바를 가로로 쌓아주기

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,27 +1,42 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import styled from "styled-components";
+
+const Header = styled.header``;
+
+const List = styled.ul`
+    display: flex;
+    &:hover {
+        background-color: blue;
+    }
+`;
+
+const Item = styled.li``;
+
+const SLink = styled(Link)``;
 
 export default () => (
-    <header>
-        <ul>
-            <li>
-                <a href="/">공지사항</a>
-            </li>
-            <li>
-                <a href="/workbook">학습지게시판</a>
-            </li>
-            <li>
-                <a href="/lecture">강좌게시판</a>
-            </li>
-            <li>
-                <a href="/study">스터디게시판</a>
-            </li>
-            <li>
-                <a href="/consultant">상담게시판</a>
-            </li>
-            <li>
-                <a href="/qna">오류문의게시판</a>
-            </li>
+    <Header>
+        <List>
+            <Item>
+                <SLink to="/">공지사항</SLink>
+            </Item>
+            <Item>
+                <SLink to="/workbook">학습지게시판</SLink>
+            </Item>
+            <Item>
+                <SLink to="/lecture">강좌게시판</SLink>
+            </Item>
+            <Item>
+                <SLink to="/study">스터디게시판</SLink>
+            </Item>
+            <Item>
+                <SLink to="/consultant">상담게시판</SLink>
+            </Item>
+            <Item>
+                <SLink to="/qna">오류문의게시판</SLink>
+            </Item>
 
-        </ul>
-    </header>
+        </List>
+    </Header>
 );
--- src/Component/Router.js
+++ src/Component/Router.js
@@ -13,18 +13,23 @@ import Study from "../Routes/Study";
 import Consultant from "../Routes/Consultant";
 import qna from "../Routes/Errorqna";
 
+import Header from "./Header";
+
 export default () => (
     <Router>
+        <>
+            <Header />
+            <Switch>
+                <Route path="/" exact component={Notice} />
+                <Route path="/workbook" exact component={Workbook} />
+                <Route path="/lecture" exact component={Lecture} />
+                <Route path="/study" exact component={Study}/>
+                <Route path="/consultant" exact component={Consultant}/>
+                <Route path="/qna" exact component={qna}/>
 
-        <Switch>
-            <Route path="/" exact component={Notice} />
-            <Route path="/workbook" exact component={Workbook} />
-            <Route path="/lecture" exact component={Lecture} />
-            <Route path="/study" exact component={Study}/>
-            <Route path="/consultant" exact component={Consultant}/>
-            <Route path="/qna" exact component={qna}/>
-
-            <Redirect from="*" to="/"/>
-        </Switch>
+                <Redirect from="*" to="/"/>
+            </Switch>
+        </>
     </Router>
+    
 );
