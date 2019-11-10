8a6eb0e5b278be4007057ad0b0ea67405564d9c0
Jay <psatdoctor@naver.com>
2019-11-10 09:52:43 +0900

    function to Class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>AAA</h1>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>test</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
